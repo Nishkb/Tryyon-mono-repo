731eb0a37e8bf6cbea1993b8336444596935c146
Ankit Rawani <ankitgdes@gmail.com>
2022-08-04 11:29:35 +0530

    Add: packages, middlewares and route for uploading files

---
+++ pages/api/upload/index.js
@@ -0,0 +1,74 @@
+import async from 'async';
+
+import handleResponse from '../../../utils/helpers/handleResponse';
+import runMiddleware from '../../../utils/helpers/runMiddleware';
+import auth from '../../../utils/middlewares/auth';
+import aws from 'aws-sdk';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+const spacesEndpoint = new aws.Endpoint(process.env.SPACES_ENDPOINT);
+
+const s3 = new aws.S3({
+  endpoint: spacesEndpoint,
+  credentials: {
+    accessKeyId: process.env.SPACES_ACCESS_KEY,
+    secretAccessKey: process.env.SPACES_SECRET
+  }
+});
+
+const uploadHandler = multer({
+  storage: multerS3({
+    s3,
+    bucket: `tryyon-files`,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      cb(null, `${Date.now().toString()}-${file.originalname}`);
+    }
+  })
+}).array('image', 5);
+
+const handler = async (req, res) => {
+  await runMiddleware(req, res, auth);
+  await runMiddleware(req, res, uploadHandler);
+
+  if (req.method == 'POST') {
+    async.auto(
+      {
+        upload: [
+          async () => {
+            if (!req.files) {
+              throw new Error(
+                JSON.stringify({
+                  errorkey: 'upload',
+                  body: {
+                    status: 500,
+                    data: {
+                      message: 'Error while uploading files'
+                    }
+                  }
+                })
+              );
+            }
+
+            return {
+              message: 'Success',
+              body: req.files
+            };
+          }
+        ]
+      },
+      handleResponse(req, res, 'upload')
+    );
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+};
+
+export default handler;
